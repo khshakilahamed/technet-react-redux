f14b375aeb31ce20a0b728f7b8443595c375431b
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-07-12 02:02:03 +0600

    react thunk & firebase authentication

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,3 +1,15 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const api = createApi({});
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  tagTypes: ['comments'],
+  endpoints: () => ({}),
+});
+
+// export const {
+//   useGetProductsQuery,
+//   useSingleProductQuery,
+//   usePostCommentMutation,
+//   useGetCommentQuery,
+// } = api;
